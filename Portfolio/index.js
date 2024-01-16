6b40faf00cdeb6824730b31e2e92ae7bfc9ce47c
Pavan Kumar Muppuri <155610590+pavankumarmuppuri@users.noreply.github.com>
2024-01-16 12:19:48 +0530

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,21 @@
+function scrolldivabout(){
+    var elem = document.getElementById("about");
+    elem.scrollIntoView();
+}
+function scrolldivskills(){
+    var elem = document.getElementById("skills");
+    elem.scrollIntoView();
+}
+function scrolldivprojects(){
+    var elem = document.getElementById("projects");
+    elem.scrollIntoView();
+}
+function scrolldivcontact(){
+    var elem = document.getElementById("contact");
+    elem.scrollIntoView();
+}
+function scrolldivhome(){
+    var elem = document.getElementById("home");
+    elem.scrollIntoView();
+}
+
